372fa8313323f769918304d6f81ba5f9d5d12054
PetterBakke <74598862+PetterBakke@users.noreply.github.com>
2024-01-23 20:57:02 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="mt-12 mb-12 text-3xl text-center dark:text-white">Hello</h1>
+    </main>
+  );
+}
